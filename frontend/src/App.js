cb3dc6090a2fab30fda575de080699c7702f3c8f
Mohib Akoum <2431135A@student.gla.ac.uk>
2022-02-13 11:22:38 +0000

    fixing merge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
@@ -7,34 +8,36 @@ import AppState from "./store/AppState";
 //Pages
 import SignUp from "./pages/Signup";
 import Login from "./pages/Login";
-import MapChart from "./Dashboard/MapChart";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./Dashboard/MapChart";
+import ReactTooltip from "react-tooltip";
 import HomePage from "./Dashboard/HomePage";
 
-import ReactTooltip from "react-tooltip";
-
 function App() {
-  const [content, setContent] = useState("");
-  return (
-    <Router>
-      <AppState>
-        <Switch>
-          <Route exact path="/">
-            <SignUp />
-          </Route>
-          <Route exact path="/signup">
-            <SignUp />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-        </AppState>
-    </Router>
-  );
+   const [content, setContent] = useState("");
+   return (
+      <Router>
+         <AppState>
+            <Switch>
+               <Route exact path="/">
+                  <HomePage />
+               </Route>
+               <Route exact path="/">
+                  <SignUp />
+               </Route>
+               <Route exact path="/signup">
+                  <SignUp />
+               </Route>
+               <Route exact path="/login">
+                  <Login />
+               </Route>
+               <Route exact path="/dashboard">
+                  <Dashboard setTooltipContent={setContent} />
+                  <ReactTooltip>{content}</ReactTooltip>
+               </Route>
+            </Switch>
+         </AppState>
+      </Router>
+   );
 }
 
 export default App;
--- frontend/src/Dashboard/HomePage.js
+++ frontend/src/Dashboard/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import tweet from "./tweet.png";
+import "../App.css";
 
 import Carousel from "react-bootstrap/Carousel";
 
@@ -8,22 +9,14 @@ const HomePage = () => {
    return (
       <Carousel>
          <Carousel.Item>
-            <img
-               className="d-block w-100"
-               src="holder.js/800x400?text=First slide&bg=373940"
-               alt="First slide"
-            />
+            <img className="d-block w-100" src={tweet} alt="First slide" />
             <Carousel.Caption>
                <h3>First slide label</h3>
                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
          </Carousel.Item>
          <Carousel.Item>
-            <img
-               className="d-block w-100"
-               src="holder.js/800x400?text=Second slide&bg=282c34"
-               alt="Second slide"
-            />
+            <img className="d-block w-100" src={tweet} alt="Second slide" />
 
             <Carousel.Caption>
                <h3>Second slide label</h3>
@@ -31,11 +24,7 @@ const HomePage = () => {
             </Carousel.Caption>
          </Carousel.Item>
          <Carousel.Item>
-            <img
-               className="d-block w-100"
-               src="holder.js/800x400?text=Third slide&bg=20232a"
-               alt="Third slide"
-            />
+            <img className="d-block w-100" src={tweet} alt="Third slide" />
 
             <Carousel.Caption>
                <h3>Third slide label</h3>
