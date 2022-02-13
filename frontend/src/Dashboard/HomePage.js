60948e2da1d48f370a50f5255037d5f8a01f0039
Mohib Akoum <2431135A@student.gla.ac.uk>
2022-02-13 12:13:33 +0000

    set up homepage

--- frontend/src/Dashboard/HomePage.js
+++ frontend/src/Dashboard/HomePage.js
@@ -1,40 +1,107 @@
 import React from "react";
 
 import tweet from "./tweet.png";
-import "../App.css";
+import map from "./map.png";
+import weatherAndNews from "./weatherAndNews.png";
+import { Link } from "react-router-dom";
 
+import "../App.css";
 import Carousel from "react-bootstrap/Carousel";
 
 const HomePage = () => {
+   const goToSignUp = () => {};
    return (
-      <Carousel>
-         <Carousel.Item>
-            <img className="d-block w-100" src={tweet} alt="First slide" />
-            <Carousel.Caption>
-               <h3>First slide label</h3>
-               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-         </Carousel.Item>
-         <Carousel.Item>
-            <img className="d-block w-100" src={tweet} alt="Second slide" />
+      <>
+         <Carousel>
+            <Carousel.Item>
+               <img className="d-block w-100 h100" src={tweet} alt="First slide" />
+               <Carousel.Caption>
+                  <h3
+                     style={{
+                        color: "white",
+                        fontSize: "5em",
+                        marginBottom: "7%",
+                     }}
+                  >
+                     This website does just that!
+                  </h3>
+                  {/* <p style={{ fontSize: "3em", marginBottom: "8.5%" }}>
+                  This website fulfills the need of keeping up to date with your team
+                  members
+               </p> */}
+               </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+               <img className="d-block w-100" src={map} alt="Second slide" />
 
-            <Carousel.Caption>
-               <h3>Second slide label</h3>
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-         </Carousel.Item>
-         <Carousel.Item>
-            <img className="d-block w-100" src={tweet} alt="Third slide" />
+               <Carousel.Caption>
+                  <h3
+                     style={{
+                        //  color: "white",
+                        fontSize: "5em",
+                        marginBottom: "7%",
+                     }}
+                  >
+                     By utilizing a map view
+                  </h3>
+               </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+               <img className="d-block w-100" src={weatherAndNews} alt="Third slide" />
 
-            <Carousel.Caption>
-               <h3>Third slide label</h3>
-               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-         </Carousel.Item>
-      </Carousel>
-      //   <div>
-      //      <img src={tweet} className={"image"} alt="tweet" />
-      //   </div>
+               <Carousel.Caption>
+                  <h3
+                     style={{
+                        //  color: "white",
+                        fontSize: "5em",
+                        marginBottom: "7%",
+                     }}
+                  >
+                     Showing you real time weather reports AND real time news reports
+                  </h3>
+               </Carousel.Caption>
+            </Carousel.Item>
+         </Carousel>
+         <div
+            style={{
+               marginTop: "1.6%",
+               display: "flex",
+               justifyContent: "space-evenly",
+               //    position: "absolute",
+            }}
+         >
+            <button
+               className="button"
+               style={{ backgroundColor: "black", color: "white", padding: "1%" }}
+               onClick={() => goToSignUp()}
+            >
+               <Link
+                  className="button"
+                  style={{
+                     color: "white",
+                  }}
+                  to="/signup"
+               >
+                  Sign Up
+               </Link>
+            </button>
+            <button
+               className="button"
+               style={{ backgroundColor: "black", color: "white", padding: "1%" }}
+               onClick={() => goToSignUp()}
+            >
+               <Link
+                  className="button"
+                  style={{
+                     color: "white",
+                  }}
+                  to="/login"
+               >
+                  Log In
+               </Link>
+            </button>
+         </div>
+      </>
    );
 };
 
