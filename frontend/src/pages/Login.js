04365f210b0378db3b06cec67379e209cb31de96
Evelyn Anyebe <anyebeevelyn@gmail.com>
2022-08-17 18:26:43 +0100

    Update login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,7 +11,7 @@ import { formatErrors } from "../utils/error.utils";
 import { Link, useHistory } from "react-router-dom";
 
 export const Login = () => {
-    const { login } = useContextGetter();
+  const { login } = useContextGetter();
   //const [state, setState] = useState(initial_state);
   const history = useHistory();
   const validate = Yup.object().shape({
@@ -30,17 +30,18 @@ export const Login = () => {
   //   }))
   // }
 
-
-  const handleLogin= async (values, { setSubmitting, resetForm }) => {
+  const handleLogin = async (values, { setSubmitting, resetForm }) => {
     try {
       const res = await API.post(`/login`, values);
-      if (res.status===202) {
+      if (res.status === 202) {
         login(res.data.data);
         alert("Login successful");
-        setTimeout(() => { history.replace("/dashboard") }, 3000)
+        setTimeout(() => {
+          history.replace("/dashboard");
+        }, 3000);
       }
     } catch (e) {
-      alert(formatErrors(e))
+      alert(formatErrors(e));
     } finally {
       window.scrollTo(0, 0);
       setSubmitting(false);
@@ -60,33 +61,31 @@ export const Login = () => {
           onSubmit={handleLogin}
         >
           {({ handleSubmit, isSubmitting }) => (
-            <div><form onSubmit={handleSubmit}>
-              <InputField
-                label="Email"
-                name="email"
-                type="email"
-                placeholder="Email"
-              />
-              <InputField
-                label="Password"
-                name="password"
-                type="password"
-                placeholder="Password"
-              />
             <div>
-              <Button
-                type="submit"
-                disabled={isSubmitting}
-              >
-                {!isSubmitting ? (
-                  "Login"
-                ) : (
-                  <Spinner animation="border" variant="light" />
-                )}
-              </Button>
-              </div>
-            </form>
-              <p className='text-sm text-white p-2'>
+              <form onSubmit={handleSubmit}>
+                <InputField
+                  label="Email"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                />
+                <InputField
+                  label="Password"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                />
+                <div>
+                  <Button type="submit" disabled={isSubmitting}>
+                    {!isSubmitting ? (
+                      "Login"
+                    ) : (
+                      <Spinner animation="border" variant="light" />
+                    )}
+                  </Button>
+                </div>
+              </form>
+              <p className="text-sm text-white p-2">
                 Don't have an account?{" "}
                 <span>
                   <Link to="/signup"> SignUp </Link>
